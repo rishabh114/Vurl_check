fc1c8951d47d2b1ca50dcda6731f7b6391bdd382
Rishabh Saxena <73388343+rishabh114@users.noreply.github.com>
2024-08-06 14:30:28 +0530

    Update css_v.ts

--- css_v.ts
+++ css_v.ts
@@ -2,13 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<div [innerHTML]="getUserInput()"></div>',
+  template: '<div [innerHTML]="userInput"></div>',
 })
 export class AppComponent {
+  // Source: User input
   userInput: string = '<img src=x onerror=alert(1)>';
-
-  getUserInput(): string {
-    // Unsafe direct assignment, more detectable by static analysis tools
-    return this.userInput;
-  }
 }
