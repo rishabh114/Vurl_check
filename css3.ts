570b163d10646b5a7688fdff3a2a4a49f7bec08a
Rishabh Saxena <73388343+rishabh114@users.noreply.github.com>
2024-08-09 11:17:26 +0530

    Update css3.ts

--- css3.ts
+++ css3.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-config',
-  template: `<p>API Key: {{ apiKey }}</p>`
+  selector: 'app-deserialization',
+  template: `<button (click)="fetchData()">Fetch Data</button> <p>{{data}}</p>`
 })
-export class ConfigComponent {
-  apiKey: string = 'YOUR_API_KEY_HERE'; // Sensitive information exposed
+export class DeserializationComponent {
+  constructor(private http: HttpClient) {}
+  public data:any = ''
+  fetchData() {
+    this.http.get('/api/data').subscribe(response => {
+      // Directly deserializing data from the server
+      this.data = response;
+      //const deserializedData = JSON.parse(response as any); // Sink: Insecure deserialization
+      //console.log('Deserialized Data:', deserializedData);
+    });
+  }
 }
